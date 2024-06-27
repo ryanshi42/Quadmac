2bc9290c009db9675f33f2833488cfc6d27e27b4
ryan.shi <ryan.shi@bytedance.com>
2024-06-27 16:13:09 +1000

    update about, implement nonmonic / answer checking

--- src/pages/games/game.tsx
+++ src/pages/games/game.tsx
@@ -12,7 +12,9 @@ export default function Game(props) {
 
   const [score, setScore] = createSignal(0);
   const [left, setLeft] = createSignal("");
-  const [right, setRight] = createSignal("");
+  const [nonMonicLeft, setNonMonicLeft] = createSignal("1");
+  const [right, setRight] = createSignal("1");
+  const [nonMonicRight, setNonMonicRight] = createSignal("");
   const [poly, setPoly] = createSignal('\\LaTeX');
   const [timerExpired, setTimerExpired] = createSignal(false);
 
@@ -55,41 +57,76 @@ export default function Game(props) {
       katex.render(math_input_html[i].innerHTML, math_input_html[i]); 
     }
 
-
-    document.getElementById("leftSource").focus();
+    if (!is_monic) {
+      document.getElementById("nonMonicLeftSource").focus();
+    } else {
+      document.getElementById("leftSource").focus();
+    }
     startTimer();
   });
 
   const generatePolynomial = function(): [string, string] {
     let newLeft = 0;
+    let newNonMonicLeft = 1;
+    let hasNonMonicRolled = false;
     while (newLeft === 0) {
       newLeft = Math.floor(Math.random() * (rrange - lrange)) + lrange;
     };
+
+    if (!is_monic) {
+      while (newNonMonicLeft === 0 || hasNonMonicRolled === false) {
+        newNonMonicLeft = Math.floor(Math.random() * (rrange - lrange)) + lrange;
+        hasNonMonicRolled = true;
+      }; 
+    }
+
+    hasNonMonicRolled = false;
+
     let newRight = 0;
+    let newNonMonicRight = 1;
     while (newRight === 0) {
       newRight = Math.floor(Math.random() * (rrange - lrange)) + lrange;
     }
+
+    if (!is_monic) {
+      while (newNonMonicRight === 0 || hasNonMonicRolled === false) {
+        newNonMonicRight = Math.floor(Math.random() * (rrange - lrange)) + lrange;
+        hasNonMonicRolled = true;
+      }; 
+    }
+
+    console.log(newNonMonicLeft, newNonMonicRight);
+
     let first = "x^2 ";
+    if (newNonMonicLeft * newNonMonicRight !== 1) {
+      first = String(newNonMonicLeft * newNonMonicRight) + first;
+    }
     let second = "";
-    if (newLeft + newRight != 0) {
-      if (newLeft + newRight < 0) {
-        second = "- " + String(Math.abs(newLeft + newRight)) + "x ";
+    
+    let sum_roots = newNonMonicRight * newLeft + newNonMonicLeft * newRight;
+    let prod_roots = newLeft * newRight;
+
+    if (sum_roots != 0) {
+      if (sum_roots < 0) {
+        second = "- " + String(Math.abs(sum_roots)) + "x ";
       } else {
-        second = "+ " + String(Math.abs(newLeft + newRight)) + "x ";
+        second = "+ " + String(Math.abs(sum_roots)) + "x ";
       }
     }
+
     let third = "";
-    if (newLeft * newRight != 0) {
-      if (newLeft * newRight < 0) {
-        third = "- " + String(Math.abs(newLeft * newRight));
+    if (prod_roots != 0) {
+      if (prod_roots < 0) {
+        third = "- " + String(Math.abs(prod_roots));
       } else {
-        third = "+ " + String(Math.abs(newLeft * newRight));
+        third = "+ " + String(Math.abs(prod_roots));
       }
     }
 
     // KaTeX worked like a charm <3
     setPoly(first + second + third);
     katex.render(poly(), document.getElementById("math"));
+
     // console.log(newLeft, newRight);
     let leftStr = String(newLeft);
     let rightStr = String(newRight);
@@ -104,15 +141,26 @@ export default function Game(props) {
 
   const leftHandler = function(e) {
     setLeft(e.target.value);
-    checkAnswer(left(), right());
+    checkAnswer(nonMonicLeft() + left(), nonMonicRight() + right());
+  };
+
+  const nonMonicLeftHandler = function(e) {
+    setNonMonicLeft(e.target.value);
+    checkAnswer(nonMonicLeft() + left(), nonMonicRight() + right());
   };
 
   const rightHandler = function(e) {
     setRight(e.target.value);
-    checkAnswer(left(), right());
+    checkAnswer(nonMonicLeft() + left(), nonMonicRight() + right());
+  };
+
+  const nonMonicRightHandler = function(e) {
+    setNonMonicRight(e.target.value);
+    checkAnswer(nonMonicLeft() + left(), nonMonicRight() + right());
   };
 
   const checkAnswer = (l: string, r: string) => {
+    // Have to have pairs like (2, 2)(4, 4) or (1, 1)(8, 8) or (8, 8)(1, 1)
     if ((l == leftAnswer && r == rightAnswer) || (r == leftAnswer && l == rightAnswer)) {
       [leftAnswer, rightAnswer] = generatePolynomial();
       setScore(score() + 1);
@@ -120,7 +168,11 @@ export default function Game(props) {
       (document.getElementById("rightSource") as HTMLInputElement).value = "";
 
       // Focus on left source
-      document.getElementById("leftSource").focus();
+      if (!is_monic) {
+        document.getElementById("nonMonicLeftSource").focus();
+      } else {
+        document.getElementById("leftSource").focus();
+      }
     }
   };
 
@@ -142,18 +194,41 @@ export default function Game(props) {
           <br/>
 
           <div>
-            <span class="math-input">( x</span><input 
-              style="margin: 5px; width: 30px"
-              oninput={leftHandler}
-              id="leftSource"
-              > 
-            </input> <span class="math_input">)</span>
-            <span class="math-input">( x</span><input 
-              style="margin: 5px; width: 30px"
-              oninput={rightHandler}
-              id="rightSource"
-              >
-            </input> <span class="math_input">)</span>
+            <span class="math-input">( </span>
+            <Switch>
+              <Match when={!is_monic}>
+                <input 
+                  style="margin: 5px; width: 15px"
+                  oninput={nonMonicLeftHandler}
+                  id="nonMonicLeftSource"
+                /> 
+              </Match>
+            </Switch>
+            <span class="math-input">x</span>
+            <input 
+            style="margin: 5px; width: 30px"
+            oninput={leftHandler}
+            id="leftSource"
+            /> 
+            <span class="math-input">) </span>
+
+            <span class="math-input">( </span>
+              <Switch>
+              <Match when={!is_monic}>
+                <input 
+                  style="margin: 5px; width: 15px"
+                  oninput={nonMonicRightHandler}
+                  id="nonMonicRightSource"
+                /> 
+              </Match>
+            </Switch>
+            <span class="math-input">x</span>
+            <input 
+            style="margin: 5px; width: 30px"
+            oninput={rightHandler}
+            id="rightSource"
+            /> 
+            <span class="math-input">) </span>
           </div>
         </Match>
       </Switch>
